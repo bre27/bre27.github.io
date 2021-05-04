b74a3a1dfff66dee0eabb98416f38b18e83eb758
bre27 <77708791+bre27@users.noreply.github.com>
2021-05-04 17:22:23 -0600

    looked at syles.css/Finished Chapter 21 exercises

--- projects/ch21/ch21-1.js
+++ projects/ch21/ch21-1.js
@@ -1,8 +1,12 @@
 // 1. 
 var friends = ["Zeida","Madi", "Nathan","Zackary"]
-
-// 2. 
-
-
-// 3.
+alert(nathan);
+var name = "Brianna Sammons"
+if(name === Jennifer) {alert("That's my name too!");}
+var myVariable = 7;
+if(myvariable > 5) {
+    alert ("upper")
+} else {
+    alert ("lower");
+}
            
